ddd2e15256702259963172d997878fbd94020bc0
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-10 22:24:41 -0300

    Adicionado validacoes e relacionamento

--- src/main/java/br/com/supera/game/store/model/ShoppingCart.java
+++ src/main/java/br/com/supera/game/store/model/ShoppingCart.java
@@ -1,18 +1,55 @@
 package br.com.supera.game.store.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
 public class ShoppingCart {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private long userId;
+    @OneToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private User user;
 
-    private Collection<Product> products;
+    @NotNull
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotEmpty
+    @OneToMany(mappedBy = "shoppingCart")
+    @JoinColumn(name = "shopping_cart_id")
+    private Collection<CartProduct> cartProducts = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Collection<CartProduct> getCartProducts() {
+        return cartProducts;
+    }
+
+    public void setCartProducts(Collection<CartProduct> cartProducts) {
+        this.cartProducts = cartProducts;
+    }
 }
