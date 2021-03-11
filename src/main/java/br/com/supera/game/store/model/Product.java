29c660189080b3937b70280edc6c9ebd5f018817
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-10 22:24:10 -0300

    Adicionado validacoes

--- src/main/java/br/com/supera/game/store/model/Product.java
+++ src/main/java/br/com/supera/game/store/model/Product.java
@@ -4,21 +4,65 @@ import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Product {
 
    @Id
-   @GeneratedValue
-   public long id;
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private long id;
 
-   public String name;
+   @NotBlank
+   private String name;
 
-   public BigDecimal price;
+   @NotNull
+   private BigDecimal price;
 
-   public short score;
+   private short score;
 
-   public String image;
+   private String image;
 
+   public long getId() {
+      return id;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public BigDecimal getPrice() {
+      return price;
+   }
+
+   public void setPrice(BigDecimal price) {
+      this.price = price;
+   }
+
+   public short getScore() {
+      return score;
+   }
+
+   public void setScore(short score) {
+      this.score = score;
+   }
+
+   public String getImage() {
+      return image;
+   }
+
+   public void setImage(String image) {
+      this.image = image;
+   }
 }
