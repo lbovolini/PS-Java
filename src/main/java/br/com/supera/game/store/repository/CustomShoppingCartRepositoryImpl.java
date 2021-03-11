98a857f1407dd2d4c9f8a0fbf441c28a348b611b
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-10 22:22:38 -0300

    Adicionado docker

--- src/main/java/br/com/supera/game/store/repository/CustomShoppingCartRepositoryImpl.java
+++ src/main/java/br/com/supera/game/store/repository/CustomShoppingCartRepositoryImpl.java
@@ -1,10 +1,19 @@
 package br.com.supera.game.store.repository;
 
 import br.com.supera.game.store.model.ShoppingCart;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public class ShoppingCartRepositoryImpl implements ShoppingCartRepository {
+
+    @PersistenceContext(name = "productDS")
+    private EntityManager entityManager;
 
     @Override
     public void delete(long id) {
@@ -18,7 +27,9 @@ public class ShoppingCartRepositoryImpl implements ShoppingCartRepository {
 
     @Override
     public ShoppingCart save(ShoppingCart shoppingCart) {
-        return null;
+        entityManager.persist(shoppingCart);
+
+        return shoppingCart;
     }
 
     @Override
