3a96acfd7d05142b111eb5dd6dfd0b1968a53225
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-10 22:25:55 -0300

    Adicionado modelo de relacionamento

---
+++ src/main/java/br/com/supera/game/store/model/CartProduct.java
@@ -0,0 +1,53 @@
+package br.com.supera.game.store.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class CartProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "shopping_cart_id", insertable = false, updatable = false)
+    private ShoppingCart shoppingCart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    private Product product;
+
+    @NotNull
+    @Column(name = "shopping_cart_id")
+    private Long shoppingCartId;
+
+    @NotNull
+    @Column(name = "product_id")
+    private Long productId;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Long getShoppingCartId() {
+        return shoppingCartId;
+    }
+
+    public void setShoppingCartId(Long shoppingCartId) {
+        this.shoppingCartId = shoppingCartId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
